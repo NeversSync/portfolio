a02ec5ee44f8d8ffdd5a1418a1d3b7630ea76ff1
nicky evers <livelyevers@gmail.com>
2017-07-31 16:25:20 -0700

    boiler plate

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router,
+         Route,
+         Switch,
+         Redirect
+} from 'react-router-dom';
+import styled from 'styled-components';
+import './App.css';
+
+const Footer = styled.div`
+  text-align: center;
+  color: lightgrey;
+  padding: 1em 0;
+  min-height: 1%;
+`;
+
+const Routes = (
+  <Switch>
+    {/* <Route exact path="/feed" render={() => <Feed />} />
+    <Route exact path="/" render={() => <Login />} />
+    <Route path="/AddPic" render={() => <AddPic />} /> */}
+    <Redirect to="/" />
+  </Switch>
+);
+
+class App extends Component {
+  render() {
+    return (
+        <Router>
+        <div>
+          <main>
+            {Routes}
+          </main>
+          <Footer>
+            &copy; NickyEvers 2017
+          </Footer>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import 'reset-css/reset.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
