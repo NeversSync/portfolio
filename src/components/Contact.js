4f7203d857029770e802588109aa63e7af1e1cbd
nicky evers <livelyevers@gmail.com>
2017-08-27 21:55:40 -0700

    Create contact component boiler plate

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import resumeLink from '../assets/Nicky-Evers-Tech-Resume.pdf';
+
+function Contact() {
+  return (
+    <div>
+      <a href={resumeLink}>resume</a>
+    </div>
+  );
+}
+
+export default Contact;
