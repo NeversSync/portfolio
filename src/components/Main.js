d0980d1db9847e79af473f68ab4d6698c10252af
nicky evers <livelyevers@gmail.com>
2017-08-27 21:54:20 -0700

    Add Main component as secondary entry point

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Landing from './Landing';
+import About from './About';
+import Contact from './Contact';
+
+function Main() {
+  return (
+    <div>
+      <Landing/>
+      <About/>
+      <Contact/>
+    </div>
+  );
+}
+
+export default Main;
