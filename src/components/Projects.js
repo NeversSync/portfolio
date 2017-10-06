cb6653d2e2acc79c26a10867f4b97749975d9807
nicky evers <livelyevers@gmail.com>
2017-10-06 13:09:30 -0700

    Reduce projects to two and add Title

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
-import tableauImg from '../assets/img/landing-page-tableau.png';
+import tableauImg from '../assets/img/tableau-screen-shot.png';
 import gongFuImg from '../assets/img/gong-fu-tea-screenshot.png';
-import keyDownImg from '../assets/img/keydown-screenshot.png';
 
 const Wrapper = styled.div`
   margin: 30px 0 0 0;
   width: 100%;
-  height: 650px;
+  height: 400px;
   font-family: 'Raleway', sans-serif;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-evenly;
   align-content: space-evenly;
+`;
+
+const Title = styled.div`
+  font-family: 'Raleway', sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 170px;
+  font-size: 2em;
+  color: white;
+  background-color: hsla(187, 71%, 41%, 0.79);
 `;
 
 const Project = styled.div`
@@ -22,6 +32,7 @@ const Project = styled.div`
   align-content: space-evenly;
   width: 40%;
   height: 300px;
+  margin: 10px;
 `;
 
 const Tableau = Project.extend`
@@ -37,6 +48,7 @@ text-align: center;
 font-size: .90em;
 font-weight: 600;
 line-height: 1.5em;
+margin: 2px;
 `;
 
 const SubHeader = styled.h3`
@@ -44,10 +56,14 @@ text-align: center;
 font-size: .75em;
 font-weight: 300;
 line-height: 1.2em;
+margin: 2px;
 `;
 
 const ImgWrapper = styled.div`
 width: 400px;
+height: 300px;
+// border: .2px solid black;
+box-shadow: .5px .5px 8px .2px grey;
 `;
 
 
@@ -58,28 +74,21 @@ width: 400px;
 
 function Projects() {
   return (
-    <Wrapper>
-      <Tableau>
-        <Header>Tableau</Header>
-        <SubHeader>React.js/Redux, Node.js, Express.js, AWS S3, <br/>MongoDB, Mongoose, Heroku, Mlab</SubHeader>
-        <ImgWrapper><img style={{ width: '100%', height: '100%'}}src={tableauImg} alt=''/></ImgWrapper>
-      </Tableau>
-      <Project>
-        <Header>Gong Fu Tea</Header>
-        <SubHeader>Mobile-first, Node.js, Express.js, Google Maps API, <br/> PostgreSQL, MongoDB, Mongoose, TDD, Jquery, Heroku</SubHeader>
-        <ImgWrapper><img style={{ width: '100%', height: '100%'}}src={gongFuImg} alt=''/></ImgWrapper>
-      </Project>
-      <Project>
-        <Header>KeyDown</Header>
-        <SubHeader>Vanilla Javascript, HTML 5, CSS</SubHeader>
-        <ImgWrapper><img style={{ width: '100%', height: '100%'}}src={keyDownImg} alt=''/></ImgWrapper>
-      </Project>
-      <Project>
-      <Header>React Image Gallery</Header>
-        <SubHeader>React.js/Redux, Node.js, Express.js, MongoDB,<br/> Mongoose, Heroku, Mlab</SubHeader>
-        <ImgWrapper><img style={{ width: '100%', height: '100%'}}src={keyDownImg} alt=''/></ImgWrapper>
-      </Project>
-    </Wrapper>
+    <div>
+      <Title><h2>Projects</h2></Title>
+      <Wrapper>
+        <Tableau>
+          <Header>Tableau</Header>
+          <SubHeader>React.js/Redux, Node.js, Express.js, AWS S3, <br/>MongoDB, Mongoose, Heroku, Mlab</SubHeader>
+          <ImgWrapper><img style={{ width: '100%'}}src={tableauImg} alt=''/></ImgWrapper>
+        </Tableau>
+        <Project>
+          <Header>Gong Fu Tea</Header>
+          <SubHeader>Mobile-first, Node.js, Express.js, Google Maps API, <br/> PostgreSQL, MongoDB, Mongoose, TDD, Jquery, Heroku</SubHeader>
+          <ImgWrapper><img style={{ width: '100%'}}src={gongFuImg} alt=''/></ImgWrapper>
+        </Project>
+      </Wrapper>
+    </div>
   );
 }
 export default Projects;
