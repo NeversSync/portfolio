3ad20965151c6f40e58ea000ed4c30d3be986784
nicky evers <livelyevers@gmail.com>
2018-02-23 17:11:42 -0800

    Test componentdidmount approach to image loading sequence

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -8,7 +8,8 @@ class Landing extends Component {
     super(props);
     this.state = {
       color: 'hsla(187, 55%, 50%, 1)',
-      size: '100px'
+      size: '100px',
+      content: ''
     };
 
     this.hoverToggle = this.hoverToggle.bind(this);
@@ -16,32 +17,44 @@ class Landing extends Component {
 
   hoverToggle() {
     this.state.color === 'hsla(187, 55%, 50%, 1)' ? this.setState({ color: 'hsla(187, 70%, 35%, 1)' }) : this.setState({ color: 'hsla(187, 55%, 50%, 1)' });
-    this.state.size === '100px' ? this.setState({ size: '110px'}) : this.setState({ size: '100px'});
+    this.state.size === '100px' ? this.setState({ size: '110px' }) : this.setState({ size: '100px' });
+  }
+
+  componentDidMount() {
+    const innerContent = 
+      <div>
+        <Header>
+          <SubTitle>Web Developer</SubTitle>
+          <Title>Nicky Evers...</Title>
+        </Header><ArrowWrapper>
+          <Scrollchor to="#about" animate={{ duration: 900 }}
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+            }}>
+            <ArrowIcon
+              onMouseEnter={this.hoverToggle} onMouseLeave={this.hoverToggle}
+              style={{
+                color: this.state.color,
+                width: this.state.size,
+                height: this.state.size
+              }} />
+          </Scrollchor>
+        </ArrowWrapper>
+      </div>;
+
+    console.log('mounted');
+    this.setState({
+      content: innerContent
+    });
   }
 
   render() {
+
     return (
       <Wrapper>
         <Background>
-          <Header>
-            <SubTitle>Web Developer</SubTitle>
-            <Title>Nicky Evers</Title>
-          </Header>
-          <ArrowWrapper>
-            <Scrollchor to="#about" animate={{ duration: 900 }}
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-              }}>
-            <ArrowIcon 
-            onMouseEnter={this.hoverToggle} onMouseLeave={this.hoverToggle} 
-            style={{
-              color: this.state.color,
-              width: this.state.size,
-              height: this.state.size
-            }} />
-            </Scrollchor>
-          </ArrowWrapper>
+          {this.state.content}
         </Background>
       </Wrapper>
     );
