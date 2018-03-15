f3dae081afad42f359e82bb32b96d460fd8ee019
nicky evers <livelyevers@gmail.com>
2018-03-15 01:37:40 -0700

    Added contact button in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import resumeLink from '../assets/Nicky-Evers-Tech-Resume.pdf';
 import ResumeIcon from 'react-material-icons/icons/action/description';
-import { Wrapper, Copy, Copyright, ButtonWrapper, Button, LabelWrapper } from './styles/Footer.style';
+import EmailIcon from 'react-icons/lib/fa/envelope-square';
+import { Wrapper, Copy, Copyright, ButtonWrapper, Button, LabelWrapper, CallToAction } from './styles/Footer.style';
 
 function Footer() {
   return (
     <Wrapper>
-      <Copy>Hiring? Need help on a project? Lets talk.</Copy>
-      <ButtonWrapper>
-        <Button><a href={resumeLink} target="_blank" rel="noopener noreferrer"><LabelWrapper><ResumeIcon color={'white'} style={{ margin: '0px 4px' }} /><p>Resume</p></LabelWrapper></a></Button>
-      </ButtonWrapper>
+      <CallToAction>
+        <Copy>Hiring? Need help on a project? Lets talk.</Copy>
+        <ButtonWrapper>
+          <Button>
+            <a href='mailto:livelyevers@gmail.com' target="_blank" rel="noopener noreferrer">
+            <LabelWrapper>
+            <EmailIcon 
+              color={'white'} 
+              style={{ margin: '0px 9px 0px 0px', fontSize: '29px' }} />
+              <p>CONTACT ME</p>
+            </LabelWrapper></a>
+          </Button>
+          <Button>
+            <a href={resumeLink} target="_blank" rel="noopener noreferrer">
+            <LabelWrapper>
+              <ResumeIcon 
+                color={'white'} 
+                style={{ margin: '0px 9px 0px 0px', height: '32px', width: '32px' }} />
+                <p>RESUME</p>
+            </LabelWrapper></a></Button>
+        </ButtonWrapper>
+      </CallToAction>
       <Copyright>This site built with React.js <br />
         &copy; Nicky Evers 2017
-        </Copyright>
+      </Copyright>
     </Wrapper>
   );
 }
--- src/components/styles/Footer.style.js
+++ src/components/styles/Footer.style.js
@@ -7,17 +7,35 @@ color: white;
 min-height: 250px;
 padding-top: 2%;
 display: flex;
-justify-content: space-around;
+justify-content: space-evenly;
 flex-direction: column;
+`;
+
+const CallToAction = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  min-width: 175px;
+  min-height: 60px;
+  margin-top: 20px;
+
+  @media(min-width: 768px) {
+    flex-direction: row;
+  }
 `;
 
 const ButtonWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  min-width: 175px;
-  min-height: 60px;
-  max-height: 53px;
+  flex-direction: column;
+  margin-top: 30px;
+  
+  @media(min-width: 768px) {
+    flex-direction: row;
+    margin-top: 0;
+  }
 `;
 
 const Button = styled.button`
@@ -25,26 +43,24 @@ const Button = styled.button`
   justify-content: center;
   align-items: center; 
   font-family: 'Raleway', sans-serif;
-  width: 169px;
-  height: 53px;
-  font-size: 1.4em; 
-  padding: 6px 13px;
-  margin-top: .7rem;
+  width: 200px;
+  height: 57px;
+  font-size: 1em; 
+  padding: 6px 0px;
+  margin-bottom: 15px;
   background: linear-gradient(hsla(187, 65%, 55%, 1), hsla(187, 65%, 40%, 1));
-  color: #fff;
   border: none;
   border-bottom: 3px solid hsla(187, 65%, 30%, .7);
   box-shadow: 0px 2px 3px -1px rgba(0, 0, 0, 0.3);
   transition: .3s ease;
+  letter-spacing: 1.8px;
   cursor: pointer;
 
   @media(min-width: 768px) {
-    font-size: 1.2em;
-    margin-top: .7rem;
+    margin: 0 10px;
+    font-size: .9em;
 
     &:hover {
-      width: 172px;
-      height: 58px;
       background: linear-gradient(hsla(187, 65%, 58%, 1), hsla(187, 65%, 40%, 1));
       box-shadow: 0 4px 4px rgba(0,0,0,.25);
     }
@@ -61,19 +77,18 @@ const Button = styled.button`
 
 const LabelWrapper = styled.div`
   display: flex;
-  justify-content: space-between:
+  justify-content: space-between;
   align-items: center;
   color: white;
 `;
 
 const Copy = styled.p`
-  margin: 0 15%;
+  margin: 0 30px;
   text-align: center;
   font-size: 1.5em;
   font-style: italic;
   font-weight: 400;
   letter-spacing: .7px;
-  margin-bottom: 16px;
 
   @media(min-width:768px) {
     font-size: 1.2em;
@@ -83,12 +98,12 @@ const Copy = styled.p`
 const Copyright = Copy.extend`
   text-align: center;
   font-size: .8em;
-  margin: 60px 0 0 0;
-
-
+  margin: 50px 0 0 0;
+  
   @media(min-width:768px) {
+    margin: 70px 0 0 0;
     font-size: .9em;
   }
 `;
 
-export { Wrapper, Copy, Copyright, ButtonWrapper, Button, LabelWrapper };
+export { Wrapper, Copy, Copyright, ButtonWrapper, Button, LabelWrapper, CallToAction };
