116be6390a7daceb192c5b10832122fffdc7a9e9
Nevers <livelyevers@gmail.com>
2019-07-24 16:54:36 -0700

    Convert to Gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Layout from '../components/layout';
+import Landing from '../components/Landing';
+import About from '../components/About';
+import Projects from '../components/Projects';
+import Contact from '../components/Contact';
+import { MainWrapper, Content } from '../components/styles/Helpers';
+
+if (typeof window !== 'undefined') {
+  // eslint-disable-next-line global-require
+  require('smooth-scroll')('a[href*="#"]');
+}
+
+if (typeof window !== `undefined`) {
+  // code using window like window.location...
+}
+
+const IndexPage = () => (
+  <Layout>
+    <div className='App' id='top'>
+      <MainWrapper>
+        <Landing />
+        <Content>
+          <About />
+          <Projects />
+          <Contact />
+        </Content>
+      </MainWrapper>
+    </div>
+  </Layout>
+);
+
+export default IndexPage;
+
+// function Main() {
+//   return (
+//     <Wrapper>
+//       <Landing />
+//       <Content>
+//         <About />
+//         <Projects />
+//         <Contact/>
+//       </Content>
+//     </Wrapper>
+//   );
+// }
+
+// export default Main;
